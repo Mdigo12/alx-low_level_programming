9e95174b0587a03d8b826dbfb2b8d2b38d9d222d
Mdigo12 <mdigojacob@gmail.com>
2022-06-02 14:14:50 -0700

    4-puts.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/**
+ * int main: prints "with proper grammar, but the outcome is a piece of art,"
+ *
+ * Return: "with proper grammar, but the outcome is a piece of art," 0 otherwise
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
