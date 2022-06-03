12f62dcbe470c48efcf2ec2c16177896038f35b9
Mdigo12 <mdigojacob@gmail.com>
2022-06-03 16:54:24 -0700

    Quiz 3-9

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int n;
+	char lc;
+
+	for (n = '0'; n <= 9; n++)
+		putchar(n);
+	for (lc = 'a'; lc <= 'f'; lc++)
+		putchar(lc);
+	putchar('\n');
+
+	return (0);
+}
