7912012443c21a6f431874dff597acd4898e79d1
Mdigo12 <mdigojacob@gmail.com>
2022-06-12 03:16:34 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include <main.h>
+/**
+ * main - Entry point
+ * Description: Prints _putchar, followed by a new line
+ * Return 0 (success)
+ */
+
+int main()
+{
+	char text[8] = "_putchar";
+	int i = 0;
+	
+	for(i = 0; i < 8; i++)
+	{
+		_putchar(text[i]);
+	}
+	_putchar('\n');
+	
+	return (0);
+}
+
