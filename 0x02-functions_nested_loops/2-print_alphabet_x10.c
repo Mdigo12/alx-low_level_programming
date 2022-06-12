90d9fba39ed374f5a37e07b5ca02187f58949056
Mdigo12 <mdigojacob@gmail.com>
2022-06-12 06:12:28 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -3,7 +3,7 @@
 /**
  * print_alphabet_x10 - see description
  *
- * Description: prints 10 times the alphabet, in lowercase, followed by a new line.
+ * Description: prints 10* the alphabet, in lowercase, followed by a \n.
  *
  * Returns: void
  */
