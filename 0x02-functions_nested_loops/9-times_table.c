a6380dd0ed7d1cd70218926c026772e5cedf0e46
Mdigo12 <mdigojacob@gmail.com>
2022-06-12 11:45:31 -0700

    9-times_table.c corrections for 2 digits

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -16,6 +16,14 @@ void times_table(void)
 	{
 		for (j = 0; j <= 9; j++)
 		{
+			if ((i * j) > 10)
+			{
+				_putchar((i * j) / 10 + '0');
+				_putchar((i * j) % 10 + '0');
+				_putchar(',');
+				_putchar(' ');
+			}
+			else
 			_putchar((i * j) + '0');
 			_putchar(',');
 			_putchar(' ');
