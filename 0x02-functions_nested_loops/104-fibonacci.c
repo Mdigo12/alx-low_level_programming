e2b65b7cb79cb47ad13fa619418b49bd21e4879e
Mdigo12 <mdigojacob@gmail.com>
2022-06-13 06:20:21 -0700

    104-fibonacci.c

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Description: Prints the first fibonacci numbers starting from 1 &2
+ *
+ * Return: 0 Success
+ */
+int main(void)
+{
+	int n;
+	long i = 1;
+	long j = 1;
+	long k = 0;
+
+	printf("%ld, ", i);
+	for (n = 0; n < 97; n++)
+	{
+		k = i + j;
+		i = j;
+		j = k;
+
+		if (n == 96)
+		{
+			printf("%ld", j);
+		}
+		else
+			printf("%ld, ", j);
+	}
+	printf("\n");
+
+	return (0);
+}
