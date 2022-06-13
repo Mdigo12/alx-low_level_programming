63be36d05b0b5bb1e67c880b8014ba17186301f6
Mdigo12 <mdigojacob@gmail.com>
2022-06-13 02:26:23 -0700

    100-times_table.c update when n is 0

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -17,10 +17,11 @@ void print_times_table(int n)
 	{
 		for (i = 0; i <= n; i++)
 		{
+			_putchar('0');
+
 			for (j = 0; j <= n; j++)
 			{
 				k = i * j;
-				_putchar('0');
 
 				if (k < 10)
 				{
