082416337e6be8d35902730b405f762d35affecc
Mdigo12 <mdigojacob@gmail.com>
2022-06-13 06:12:15 -0700

    103-fibonacci.c fix betty

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -3,8 +3,8 @@
  * main - entry point
  *
  * Description: By considering the terms in the Fibonacci sequence
- * whose values do not exceed 4,000,000, 
- * the program that finds and prints the sum of the even-valued terms, 
+ * whose values do not exceed 4,000,000,
+ * the program that finds and prints the sum of the even-valued terms,
  * followed by a new line.
  *
  * Return: 0 success
