6d6a35b42f87f2000dacc7785a9f558c1e062314
Mdigo12 <mdigojacob@gmail.com>
2022-06-13 03:09:41 -0700

    101-natural.c

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,24 @@
+#include <stdio>
+
+/**
+ * main - Entry point
+ *
+ * Description: Sums multiples of 3 and 5 below 1024
+ *
+ * Return: 0 Success
+ */
+int main(void)
+{
+	int i;
+	int r = 0;
+
+	for (i <= 0; i < 1024; i++)
+	{
+		if (i % 3 == 0 || i % 5 == 0)
+		{
+			r = + i;
+		}
+	}
+	printf("%d", r);
+	printf("\n");
+}
