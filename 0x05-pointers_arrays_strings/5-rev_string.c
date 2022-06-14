db184f7cf5f0d56063031953c75632997a4c98fb
Mdigo12 <mdigojacob@gmail.com>
2022-06-13 17:32:54 -0700

    5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,10 +1,26 @@
 #include "main.h"
 
 /**
- * reset_to_98 - See dscription
- * Description: A function that takes a pointer to an int as parameter
- * and updates the value it points to to 98
- * @2: An integer input pointer
+ * rev_string - See dscription
+ * Description: A function that reverses a string
+ * followed by a new line
+ * @s: An input string
  *
  * Return: Nothing (void function)
  */
+
+void rev_string(char *s)
+{
+	int l = 0, n = 0;
+	char alx;
+
+	while (s[l] != '\0')
+		l++;
+
+	while (n < l--)
+	{
+		alx = s[n];
+		s[n++] = s[l];
+		s[l] = alx;
+	}
+}
