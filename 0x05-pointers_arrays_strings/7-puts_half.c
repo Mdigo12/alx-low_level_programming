e46a21c047fa055684c351d361910a252fa72ba3
Mdigo12 <mdigojacob@gmail.com>
2022-06-13 17:52:52 -0700

    7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,10 +1,28 @@
 #include "main.h"
 
 /**
- * reset_to_98 - See dscription
- * Description: A function that takes a pointer to an int as parameter
- * and updates the value it points to to 98
- * @2: An integer input pointer
+ * puts_half - See dscription
+ * Description: A function that prints half of a string,
+ * followed by a new line
+ * @str: An input string
  *
  * Return: Nothing (void function)
  */
+
+void puts_half(char *str)
+{
+	int l = 0, p, q;
+
+	while (str[l] != '\0')
+		l++;
+
+	if (l % 2 == 0)
+		q = l / 2;
+	else
+		q = (l + 1) / 2;
+
+	for (p = q; p < l; p++)
+		_putchar(str[p]);
+
+	_putchar('\n');
+}
