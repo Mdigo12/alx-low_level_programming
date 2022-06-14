c7287c4208ec19294a8adfd635e49b37078a9215
Mdigo12 <mdigojacob@gmail.com>
2022-06-13 18:03:47 -0700

    8-print_array.c

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,10 +1,25 @@
+#include <stdio.h>
 #include "main.h"
 
 /**
- * reset_to_98 - See dscription
- * Description: A function that takes a pointer to an int as parameter
- * and updates the value it points to to 98
- * @2: An integer input pointer
+ * print_array - See dscription
+ * Description: A function that prints n elements of an array of integers,
+ * followed by a new line
+ * @a: An input array
+ * @n: An input integer
  *
  * Return: Nothing (void function)
  */
+
+void print_array(int *a, int n)
+{
+	int p = 0;
+
+	for (; p < n; p++)
+	{
+		printf("%d", a[p]);
+		if (p < n - 1)
+			printf(", ");
+	}
+	putchar('\n');
+}
