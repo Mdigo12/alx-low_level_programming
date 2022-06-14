393562938241c1f8a23bd8b6714847b90f789798
Mdigo12 <mdigojacob@gmail.com>
2022-06-13 19:19:33 -0700

    101-keygen.c

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,18 +1,45 @@
-#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 /**
- * swap_int - See dscription
- * Description: A function that swaps the values of two integers
- * @a: An integer input pointer
- * @b: An integer input pointer
+ * main - See dscription
+ * Description: A program that generates random valid passwords
+ * for program 101-crackme.
  *
- * Return: Nothing (void function)
+ * Return: 0 (success)
  */
-void swap_int(int *a, int *b)
+int main(void)
 {
-	int swap;
+	char password[84];
+	int index = 0, sum = 0, dh1, dh2;
 
-	swap = *a;
-	*a = *b;
-	*b = swap;
+	srand(time(0));
+
+	while (sum < 2772)
+	{
+		password[index] = 33 + rand() % 94;
+		sum += password[index++];
+	}
+
+	password[index] = '\0';
+
+	if (sum != 2772)
+	{
+		dh1 = (sum - 2772) / 2;
+		dh2 = (sum - 2772) / 2;
+		if ((sum - 2772) % 2 != 0)
+			dh1++;
+
+		for (index = 0; password[index]; index++)
+		{
+			if (password[index] >= (33 + dh2))
+			{
+				password[index] -= dh2;
+				break;
+			}
+		}
+	}
+	printf("%s", password);
+	return (0);
 }
