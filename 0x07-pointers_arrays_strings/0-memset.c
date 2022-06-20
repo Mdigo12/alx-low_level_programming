2860e3af8a98edff50a3c189722d94f9897e1ad0
Mdigo12 <mdigojacob@gmail.com>
2022-06-20 13:40:34 -0700

    0-memset.c fix

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _memset() - Check description.
+ * _memset - Check description.
  * Description:  function that fills the first n bytes of the memory area
  * pointed to by s with the constant byte b
  * @s: Buffer array
