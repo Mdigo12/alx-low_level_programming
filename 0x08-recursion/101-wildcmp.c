8989aa8bb367fbb69cd19bdfbedf02ef9b6366e5
Mdigo12 <mdigojacob@gmail.com>
2022-06-22 16:28:39 -0700

    101-wildcmp.c

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,19 +1,81 @@
 #include "main.h"
+
+int _strlen(char *str);
+void iterative_wild(char **wildstr);
+char *postfix_match(char *str, char *postfix);
+int wildcmp(char *s1, char *s2);
+
 /**
- * _puts_recursion - a function that prints a string,
- * followed by a new line.
- * @s: Character string
+ * _strlen - lenth of string ignoring wildcard characters
+ * @str: input string
  *
- * Return: void
+ * Return: lenth of the string
  */
 
-void _puts_recursion(char *s)
+int _strlen(char *str)
 {
-	if (*s == '\0')
+	int len = 0;
+
+	if (*str != '\0' && *str != '*')
+		len = 1 + _strlen(s + 1);
+	return (len);
+}
+
+/**
+ * iterative_wild - goes through the string located at wildcard
+ * until it points to a non-wildcard character
+ * @wildstr: the string to be iterated
+ */
+
+void  iterative_wild(char **wildstr)
+{
+	if (**wildstr == '*')
 	{
-		_putchar('\n');
-		return;
+		(*wildstr)++;
+		iterative_wild(wildstr);
 	}
-	_putchar(*s);
-	_puts_recursion(s + 1);
+}
+/**
+ * postfix_match - checks if string matches
+ * @str: string to be matched
+ * @postfix: postfix string
+ *
+ * Return: if match, pointer to a null byte at end of postfix
+ * otherwise, pointer to the fisrt unmatched character in postfix
+ */
+
+char *postfix_match(char *str, char *postfix)
+{
+	int str_len = _strlen(str) - 1;
+	int postfix_len = _strlen(postfix) - 1;
+
+	if (*postfix == '*')
+		iterative_wild(&postfix);
+	if (*(str + str_len - postfix_len) == *postfix && *postfix != '\0')
+	{
+		postfix++;
+		return (postfix_match(str, postfix));
+	}
+	return (postfix);
+}
+/**
+ * wildcmp - compares two strings while considering wildcards
+ * @s1: Character string one
+ * @s2: Character string two
+ *
+ * Return: 1 if identical, else 0
+ */
+
+int wildcmp(char *s1, char *s2)
+{
+	if (*s2 == '*')
+	{
+		iterative_wild(&s2);
+		s2 = postfix_match(s1, s2);
+	}
+	if (*s2 == '\0')
+		return (1);
+	if (*s1 != *s2)
+		return (0);
+	return (wildcmp(++s1, ++s2));
 }
