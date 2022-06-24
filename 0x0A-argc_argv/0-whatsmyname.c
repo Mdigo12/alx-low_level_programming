5c27ce892f042e0778cb3ac3f3fe334fa467fce5
Mdigo12 <mdigojacob@gmail.com>
2022-06-24 03:08:12 -0700

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Description: Yuo
+ * @argc: arguments counter
+ * @argv: array string of arguments passed
+ *
+ * Return: void
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Description: Yuo
+ * @argc: arguments counter
+ * @argv: array string of arguments passed
+ *
+ * Return: void
+ */
+
+int main(int argc, char *argv[])
+{
+	return (0);
+}
