8bf8fadcdb271fe889307e860bac052b933e860b
Mdigo12 <mdigojacob@gmail.com>
2022-06-24 07:17:14 -0700

    100-change.c

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -2,14 +2,55 @@
 
 /**
  * main - entry point
- * Description: Yuo
+ * Description:  a program that prints the minimum number of coins
+ * to make change for an amount of money
  * @argc: arguments counter
  * @argv: array string of arguments passed
  *
- * Return: void
+ * Return: 1 if number of arguments passed is not exactly 1
+ * otherwise 0
  */
 
 int main(int argc, char *argv[])
 {
-	return (0);
+	int amt, coins = 0;
+
+	if (argc == 2)
+	{
+		amt = atoi(argv[1]);
+		if (amt < 0)
+		{
+			printf("%d\n", 0);
+			return (0);
+		}
+		if (amt % 25 >= 0)
+		{
+			coins += amt / 25;
+			amt = amt % 25;
+		}
+		if (amt % 10 >= 0)
+		{
+			coins += amt / 10;
+			amt = amt % 10;
+		}
+		if (amt % 5 >= 0)
+		{
+			coins += amt / 5;
+			amt = amt % 5;
+		}
+		if (amt % 2 >= 0)
+		{
+			coins += amt / 2;
+			amt = amt % 2;
+		}
+		if (amt % 1 >= 0)
+			coins += amt;
+		printf("%d\n", coins);
+		return (0);
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
 }
