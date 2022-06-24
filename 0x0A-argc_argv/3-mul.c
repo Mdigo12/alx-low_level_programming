d3f89f2fda7137b8f57c46d41005ba49ada38e70
Mdigo12 <mdigojacob@gmail.com>
2022-06-24 04:33:19 -0700

    3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -14,18 +14,18 @@ int main(int argc, char *argv[])
 	int i;
 	int mult = 1;
 
-	if (argc > 1)
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	else
 	{
 		for (i = 1; i < argc; i++)
 		{
 			mult *= atoi(argv[i]);
 		}
 		printf("%d\n", mult);
+		return (0);
 	}
-	else if (argc < 3)
-	{
-		printf("Error\n");
-		return (1);
-	}
-	return (0);
 }
