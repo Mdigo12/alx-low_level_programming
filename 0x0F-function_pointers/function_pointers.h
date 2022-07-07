60aa351db82fb5f4315e30ae9af77ff05ed75436
Mdigo12 <mdigojacob@gmail.com>
2022-07-06 17:48:18 -0700

    0-print_name.c

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -4,12 +4,17 @@
 /**
  * File: function_pointers.h
  *
- * Desc: Header file cointaining all protype functions
+ * Desc: Header file containing prototypes for all functions
+ * used in the 0x0F-function_pointers directory
  */
 
 #include <stdlib.h>
 
-int _putchar(char c);
+/**
+ * @name: see in fuction
+ * @f: see in function
+ */
+
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
