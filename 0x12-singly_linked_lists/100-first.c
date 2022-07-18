f248ce830acc9775e04446707ee1b39cf6e6e60a
Mdigo12 <mdigojacob@gmail.com>
2022-07-18 03:06:41 -0700

    100-first.c fix

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -9,6 +9,6 @@ void here(void) __attribute__ ((constructor));
 
 void here(void)
 {
-	printf("You're beat! and yet, you must allow,\n
-			I bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 }
