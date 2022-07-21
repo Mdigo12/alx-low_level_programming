bc71214b96a332737647de3a029eefef51f4912e
Mdigo12 <mdigojacob@gmail.com>
2022-07-21 16:21:37 -0700

    0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -23,7 +23,7 @@ unsigned int binary_to_uint(const char *b)
 	{
 		if (b[len] != '0' && b[len] != '1')
 			return (0);
-		sum += (b[len] + '0') * mult;
+		sum += (b[len] - '0') * mult;
 		mult *= 2;
 	}
 
