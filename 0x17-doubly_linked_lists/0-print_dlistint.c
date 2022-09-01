7d06f389bc4b5fbf0c0e9d4f974c6dd17e690f8c
Mdigo12 <mdigojacob@gmail.com>
2022-09-01 11:37:19 -0700

    0-print_dlistint.c

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -13,6 +13,12 @@ size_t print_dlistint(const dlistint_t *h)
 {
 	size_t nodes = 0;
 
+	if (h == NULL)
+		return (0);
+	
+	while (h->prev != NULL)
+		h = h->prev;
+
 	while (h)
 	{
 		printf("%d" h->n);
